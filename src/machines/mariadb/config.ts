057b3a53c5bf2a33fc1d925a45a55b44d0de9b6c
Kevin Dave Gerona <31026169+kdgerona@users.noreply.github.com>
2020-11-11 22:53:07 +0800

    Update config.ts

--- src/machines/mariadb/config.ts
+++ src/machines/mariadb/config.ts
@@ -2,8 +2,8 @@ const context = {
     db_host: process.env.DB_HOST || '127.0.0.1',
     db_port: process.env.DB_PORT || 9001,
     db_user: process.env.DB_USER || 'root',
-    db_pass: process.env.DB_PASSWORD || 'admin101',
-    db_name: process.env.DB_NAME || 'test',
+    db_pass: process.env.DB_PASSWORD || 'secret',
+    db_name: process.env.DB_NAME || 'db_name',
     mariadb_conn: undefined
 }
 
